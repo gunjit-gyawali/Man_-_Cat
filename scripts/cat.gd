d64132a1a7607444f02c51b967e895ef74871ec5
gunjit-gyawali <gunjitgyawali1@gmail.com>
2026-09-20 11:16:12 +0545

    random things

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -500.0
 
 @onready var animated_sprite = $AnimatedSprite2D
@@ -12,6 +12,7 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
 		
 	var direction := Input.get_axis("left", "right")
 	if direction > 0:
